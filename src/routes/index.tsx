8d145e38c44de96befd4adb52c1f5638de6fbe43
Olakunle Ogunbowale <48626541+OlaOgun4@users.noreply.github.com>
2026-07-25 07:19:40 +0100

    Update index.tsx
    Added comments to index.ts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -220,7 +220,7 @@ function TopBar({ signedIn }: { signedIn: boolean }) {
 }
 
 // ---------------------------------------------------------------------------
-// AUTH
+// AUTH OLA Added to Test deployment to Cloudflare
 // ---------------------------------------------------------------------------
 
 function AuthScreen() {
